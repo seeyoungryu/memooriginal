a8e155e809cf6eecbb1bcaeb50ab145ede439dd3
seeyoungryu <seayeux@gmail.com>
2024-07-28 02:18:38 +0900

    전체 코드 업데이트

--- src/main/java/com/example/memo/controller/MemoController.java
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -1,12 +1,41 @@
 package com.example.memo.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class MemoController {
 
-    @PostMapping("/memos")
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-        return null;
+    private final MemoService memoService;
+
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        return memoService.createMemo(requestDto);
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        return memoService.getMemos();
+    }
+
+    @GetMapping("/memos/contents")
+    public List<MemoResponseDto> getMemosByKeyword(String keyword) {
+        return memoService.getMemosByKeyword(keyword);
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        return memoService.updateMemo(id, requestDto);
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        return memoService.deleteMemo(id);
+    }
 }
---
+++ src/main/java/com/example/memo/dto/MemoResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.memo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String username;
+    private String contents;
+}
